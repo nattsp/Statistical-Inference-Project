dad64fae4505ac2e5256d173184a26422d7ea91c
nattsp <drnphillips@gmail.com>
2015-07-07 17:09:31 +1000

    Started second part of the assignment

--- Simulation.R
+++ Simulation.R
@@ -6,11 +6,17 @@ hist(rexp(1000, lambda))
 mns = NULL
 for (i in 1 : 1000) mns = c(mns, mean(runif(40)))
 hist(mns)
+median(mns)
+mean(mns)
 
 mns = NULL
+vrs = NULL
 for (i in 1 : 1000) {
-    mns = c(mns, mean(rexp(40, lambda)))
+    expdist <- rexp(40, lambda)
+    mns = c(mns, mean(expdist))
+    vrs = c(vrs, var(expdist))
 }
 hist(mns)
+hist(vrs)
 
 hist(rexp(1000, lambda))
---
+++ ToothGrowth.R
@@ -0,0 +1,8 @@
+# ToothGrowth
+
+data("ToothGrowth")
+head(ToothGrowth)
+table(ToothGrowth$supp)
+table(ToothGrowth$dose)
+table(ToothGrowth$len)
+summary(ToothGrowth)
