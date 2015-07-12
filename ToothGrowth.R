a8d7fa94e75b3c1d5a0ff44d762c8d6e750aed71
nattsp <drnphillips@gmail.com>
2015-07-12 18:09:09 +1000

    Tooth decay graph

--- ToothGrowth.R
+++ ToothGrowth.R
@@ -1,4 +1,6 @@
 # ToothGrowth
+library(dplyr)
+library(ggplot2)
 
 data("ToothGrowth")
 head(ToothGrowth)
@@ -6,3 +8,15 @@ table(ToothGrowth$supp)
 table(ToothGrowth$dose)
 table(ToothGrowth$len)
 summary(ToothGrowth)
+hist(ToothGrowth$len)
+lapply(ToothGrowth, class)
+
+Tooth <- group_by(ToothGrowth, supp)
+
+g <- ggplot(data = Tooth, aes(x = dose, y = len, colour = supp)) +
+    geom_point() + 
+    geom_smooth() +
+    labs(title = "Tooth length with dose") +
+    ylab("Tooth length")
+
+g
